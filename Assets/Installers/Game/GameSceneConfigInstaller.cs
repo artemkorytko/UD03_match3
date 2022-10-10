ace1a5d55a8845ad056cb9b15c8639d965ed77f5
Artem Korytko <a.korytko@freeplay.io>
2022-10-10 22:03:48 +0300

    add base logic for elements and board

---
+++ Assets/Installers/Game/GameSceneConfigInstaller.cs
@@ -0,0 +1,15 @@
+using Game;
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "GameSceneConfigInstaller", menuName = "Installers/GameSceneConfigInstaller")]
+public class GameSceneConfigInstaller : ScriptableObjectInstaller<GameSceneConfigInstaller>
+{
+    [SerializeField] private ElementsConfig elementsConfig;
+    [SerializeField] private BoardConfig boardConfig;
+
+    public override void InstallBindings()
+    {
+        Container.BindInstances(elementsConfig, boardConfig);
+    }
+}
---
+++ Assets/Scripts/Zenject/ProjectSetup.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Zenject
+{
+    public class ProjectSetup : IInitializable
+    {
+        private readonly ProjectSettings _projectSettings;
+
+        public ProjectSetup(ProjectSettings projectSettings)
+        {
+            _projectSettings = projectSettings;
+        }
+
+        public void Initialize()
+        {
+            Application.targetFrameRate = _projectSettings.TargetFps;
+            Input.multiTouchEnabled = _projectSettings.IsMultiTouch;
+        }
+    }
+}
